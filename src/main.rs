3e95dc3ec62bd9ebaf1d032907b780d44f1996d6
lolguy91 <retek1544@gmail.com>
2025-10-26 12:16:47 +0100

    model struct for better organization

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@
 mod math;
 mod layers;
 
+use core::panic;
+
 use rand::Rng;
 use math::tensor::Tensor;
 use layers::{prec::*,relu::*};
@@ -16,6 +18,61 @@ fn reconstruction_loss(x: f32, r: f32) -> f32{
     let diff = r - x;
     diff * diff
 }
+
+struct SimplePreceptron {
+    perc: PercLayer<f32>,
+    relu: ReluLayer<f32>,
+    perc_grad: Option<PercGradient<f32>>
+}
+
+impl SimplePreceptron {
+    fn init() -> Result<SimplePreceptron, Box<dyn std::error::Error>> {
+        let mut rng = rand::rng();
+        let perc: PercLayer<f32> = PercLayer {
+            w: Tensor::values([1, 2], &vec![rng.random_range(0.01..1.0), rng.random_range(0.01..1.0)])?,
+            b: Tensor::values([1],&vec![rng.random_range(0.01..1.0)])?,
+            saved_x: Tensor::zeroes([2]),
+        };
+
+        let relu: ReluLayer<f32> = ReluLayer{
+            saved_x: Tensor::zeroes([1]),
+        };
+
+        Ok(SimplePreceptron { perc: perc, relu: relu, perc_grad: None })
+    }
+
+    fn forwards(&mut self,x: &Tensor<f32,1>) -> Result<Tensor<f32, 1>, Box<dyn std::error::Error>> {
+        Ok(self.relu.forwards(&self.perc.forwards(x)?)?)
+    }
+
+    fn backwards(&mut self,grad: &Tensor<f32,1>) -> Result<(), Box<dyn std::error::Error>> {
+        self.perc_grad = Some(self.perc.backwards(&self.relu.backwards(grad)?)?);
+
+        Ok(())
+    }
+
+    fn apply_grad(&mut self, learning_rate: f32) -> Result<(), Box<dyn std::error::Error>> {
+            let perc_grad = match &mut self.perc_grad {
+                Some(v) => v,
+                None => {
+                    panic!("TODO: make this recoverable")
+                }
+            };
+
+            perc_grad.w_grad.mul_scalar(learning_rate);
+            perc_grad.b_grad.mul_scalar(learning_rate);
+
+            let new_weights = (&self.perc.w - &perc_grad.w_grad)?;
+            let new_biases = (&self.perc.b - &perc_grad.b_grad)?;
+
+            self.perc.w = new_weights;
+            self.perc.b = new_biases;
+
+            Ok(())
+    }
+
+}
+
 
 fn main() -> Result<(), Box<dyn std::error::Error>>{
     let training_data: [TrainingData; _] = [
@@ -37,21 +94,13 @@ fn main() -> Result<(), Box<dyn std::error::Error>>{
         },
     ];
 
-    let mut rng = rand::rng();
-    let mut perc: PercLayer<f32> = PercLayer {
-        w: Tensor::values([1, 2], &vec![rng.random_range(0.01..1.0), rng.random_range(0.01..1.0)])?,
-        b: Tensor::values([1],&vec![rng.random_range(0.01..1.0)])?,
-        saved_x: Tensor::zeroes([2]),
-    };
+    let mut model = SimplePreceptron::init()?;
 
-    let mut relu: ReluLayer<f32> = ReluLayer{
-        saved_x: Tensor::zeroes([1]),
-    };
 
-    println!("Sample 0(pre_training): {0}",relu.forwards(&perc.forwards(&training_data[0].i)?)?);
-    println!("Sample 1(pre_training): {0}",relu.forwards(&perc.forwards(&training_data[1].i)?)?);
-    println!("Sample 2(pre_training): {0}",relu.forwards(&perc.forwards(&training_data[2].i)?)?);
-    println!("Sample 3(pre_training): {0}",relu.forwards(&perc.forwards(&training_data[3].i)?)?);
+    println!("Sample 0(pre_training): {0}",model.forwards(&training_data[0].i)?);
+    println!("Sample 1(pre_training): {0}",model.forwards(&training_data[1].i)?);
+    println!("Sample 2(pre_training): {0}",model.forwards(&training_data[2].i)?);
+    println!("Sample 3(pre_training): {0}",model.forwards(&training_data[3].i)?);
 
     const EPOCHS: usize = 1000;
     const LEARNING_RATE: f32 = 0.01;
@@ -62,7 +111,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>>{
         println!("Epoch {0}, avg loss: {1}",i,sum_loss / training_data.len() as f32);
         sum_loss = 0.;
         for d in &training_data {
-            let forwards = relu.forwards(&perc.forwards(&d.i)?)?;
+            let forwards = model.forwards(&training_data[1].i)?;
 
             let loss = reconstruction_loss(forwards[[0]], d.o[[0]]);
 
@@ -70,26 +119,19 @@ fn main() -> Result<(), Box<dyn std::error::Error>>{
 
             let grad_output = Tensor::values([1],&vec![2.0 * (forwards[[0]] - d.o[[0]])])?;
 
-            let mut backwards = perc.backwards(&relu.backwards(&grad_output)?)?;
+            model.backwards(&grad_output)?;
 
-            backwards.w_grad.mul_scalar(LEARNING_RATE);
-            backwards.b_grad.mul_scalar(LEARNING_RATE);
-
-            let new_weights = (&perc.w - &backwards.w_grad)?;
-            let new_biases = (&perc.b - &backwards.b_grad)?;
-
-            perc.w = new_weights;
-            perc.b = new_biases;
+            model.apply_grad(LEARNING_RATE)?;
         }
     }
 
-    println!("Sample 0(post_training): {0}",relu.forwards(&perc.forwards(&training_data[0].i)?)?);
-    println!("Sample 1(post_training): {0}",relu.forwards(&perc.forwards(&training_data[1].i)?)?);
-    println!("Sample 2(post_training): {0}",relu.forwards(&perc.forwards(&training_data[2].i)?)?);
-    println!("Sample 3(post_training): {0}",relu.forwards(&perc.forwards(&training_data[3].i)?)?);
+    println!("Sample 0(post_training): {0}",model.forwards(&training_data[0].i)?);
+    println!("Sample 1(post_training): {0}",model.forwards(&training_data[1].i)?);
+    println!("Sample 2(post_training): {0}",model.forwards(&training_data[2].i)?);
+    println!("Sample 3(post_training): {0}",model.forwards(&training_data[3].i)?);
 
-    println!("weights: {0}",perc.w);
-    println!("biases: {0}",perc.b);
+    println!("weights: {0}",model.perc.w);
+    println!("biases: {0}",model.perc.b);
 
     Ok(())
 }
